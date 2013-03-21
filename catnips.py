3d8ce159659942873fc19207ae6dcda910e66252
Alan Medlar <amedlar@gmail.com>
2013-03-21 18:36:04 +0200

    pdfs do not overwrite one another, bag of words created, only naive cleaning performed

--- catnips.py
+++ catnips.py
@@ -3,6 +3,9 @@ import urllib2
 import os
 import getopt
 import time
+import glob
+import string
+import collections
 
 try :
     from bs4 import BeautifulSoup
@@ -15,7 +18,8 @@ except ImportError :
 options = { 
             'delay' : 0.5,
             'fetch' : False,
-            'build' : False
+            'build' : False,
+            'pdf-method' : 'pdftotext'
           }
 
 def usage() :
@@ -63,6 +67,74 @@ def rm(filename) :
     except :
         pass
 
+def get_pdftotext(pdf_name) :
+    ret = os.system("pdftotext %s" % pdf_name)
+
+    if ret != 0 :
+        print >> sys.stderr, "Error: pdftotext returned %d" % ret
+        sys.exit(-1)
+
+    f = open(pdf_name[:-3] + "txt")
+    payload = f.read()
+    f.close()
+
+    return payload
+
+def get(pdf_name) :
+    global options
+
+    method = options['pdf-method']
+
+    if method == 'pdftotext':
+        return get_pdftotext(pdf_name)
+
+    raise Exception("bad pdf method")
+
+def clean(text) :
+    goodchars = string.lowercase + "\n "
+    return filter(lambda x: x in goodchars, text.lower()).split()
+
+def bag_of_words(tokens) :
+    bow = collections.Counter()
+
+    for token in tokens :
+        bow[token] += 1
+    
+    return bow
+
+def merge_bag_of_words(bow_dict) :
+    all_words = collections.Counter()
+
+    for bow in bow_dict :
+        all_words.update(bow_dict[bow])
+
+    return all_words
+
+def write_bag_of_words(bow_dict, year) :
+    year_bow = merge_bag_of_words(bow_dict)
+    words = sorted(year_bow.keys())
+
+    # write bag of words for the year
+    f = open("%s.bow" % year, 'w')
+
+    for word in words :
+        print >> f, "%s %d" % (word, year_bow[word])
+
+    f.close()
+
+    # write bag of words per paper
+    f = open("%s_papers.bow" % year, 'w')
+
+    for word in words :
+        s = word
+
+        for pdf in bow_dict :
+            s += "\t%d" % bow_dict[pdf][word]
+
+        print >> f, s
+
+    f.close()
+
 def get_pdfs() :
     global options
     website = 'http://books.nips.cc/'
@@ -81,9 +153,10 @@ def get_pdfs() :
         print >> sys.stderr, "%s %d / %d" % (header, count, total),
         for page in pages : 
             conf = BeautifulSoup(urllib2.urlopen(website + page.get('href')))
-            
+            year = page.contents[0]
+
             for paper in filter(lambda x: x.contents[0] == '[pdf]', conf.find_all('a')) :
-                pdfs.append(paper.get('href'))
+                pdfs.append((paper.get('href'), year))
 
             count += 1
             print >> sys.stderr, "%s %d / %d" % (header, count, total),
@@ -97,6 +170,7 @@ def get_pdfs() :
     except urllib2.URLError, ue :
         print >> sys.stderr, "\n%s" % str(ue)
         sys.exit(-1)
+
 
     # create a directory, if necessary
     if not os.path.exists(directory) :
@@ -113,6 +187,7 @@ def get_pdfs() :
         
     # download all pdfs, provide progress
     count = 0
+    year_counts = collections.Counter()
     total = len(pdfs)
     header = "\rdownloading NIPS papers:"
 
@@ -120,10 +195,17 @@ def get_pdfs() :
     errcount = 0
     
     print >> sys.stderr, "%s %d / %d" % (header, count, total),
-    for pdf in pdfs :
-        pdf_name = os.path.basename(pdf)
-        pdf_dest = os.path.join(directory, pdf_name)
-        
+    for pdf,year in pdfs :
+#        pdf_name = os.path.basename(pdf)
+#        # only the most recent NIPS have good filenames
+#        # many of the early conferences use just numbers
+#        if not pdf_name.startswith('NIPS') :
+#            pdf_name = os.path.basename(os.path.dirname(pdf)).upper() + "_" + pdf_name
+#
+#        pdf_dest = os.path.join(directory, pdf_name)
+
+        pdf_dest = os.path.join(directory, "%s_%d.pdf" % (year, year_counts[year]))
+
         try :
             f = open(pdf_dest, 'w')
             f.write(urllib2.urlopen(pdf).read())
@@ -140,6 +222,7 @@ def get_pdfs() :
             errcount += 1
 
         count += 1
+        year_counts[year] += 1
         print >> sys.stderr, "%s %d / %d" % (header, count, total),
 
         time.sleep(options['delay'])
@@ -148,7 +231,27 @@ def get_pdfs() :
     errfile.close()
 
 def build_dataset() :
-    pass
+    directory = os.path.join(os.getcwd(), 'pdfs')
+    pdfs = glob.glob(os.path.join(directory, "*pdf"))
+    pdfs_by_year = {}
+
+    for pdf in pdfs :
+        year = os.path.basename(pdf).split('_')[0]
+        
+        if year not in pdfs_by_year :
+            pdfs_by_year[year] = []
+
+        pdfs_by_year[year].append(pdf)
+
+    #years = set([os.path.basename(pdf).split('_')[0] for pdf in pdfs])
+
+    for year in pdfs_by_year :
+        pdf_bow = {}
+
+        for pdf in pdfs_by_year[year] :
+            pdf_bow[os.path.basename(pdf)] = bag_of_words(clean(get(pdf)))
+
+        write_bag_of_words(pdf_bow, year)
 
 def main() :
     global options
